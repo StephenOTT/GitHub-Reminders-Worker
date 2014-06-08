d52ee6bfa818fa37ee2e0f533646ed16c9f9aae7
Stephen Russett <stephenrussett@gmail.com>
2014-06-07 19:53:51 -0500

    Qless job enhancements

--- jobs.rb
+++ jobs.rb
@@ -1,7 +1,8 @@
 require 'rest-client'
 require_relative 'reminder_validation/controller'
 require 'qless'
-# require_relative 'mongo'
+require_relative 'mongo'
+require 'json'
 
 class SendEmail
   def self.perform(job)
@@ -22,51 +23,188 @@ class SendEmail
   end
 end
 
+class MongoQueries
+		
+		def self.mongo_connection(clearCollections = false)
+			Mongo_Connection.mongo_Connect("localhost", 27017, ENV['MONGO_DB_NAME'], ENV['MONGO_DB_COLL_NAME'])
+
+			if clearCollections == true
+			Mongo_Connection.clear_mongo_collections
+			end
+		end
+
+
+
+		def self.aggregate(input)
+			Sinatra_Helpers.mongo_connection
+			Mongo_Connection.aggregate(input)
+		end
+
+
+		# Checks MongoDB to see if the user has a record/profile based on the userid
+		def self.get_user_profile(userid)
+			userProfile = self.aggregate([
+									{ "$match" => {userid: userid}},
+									{ "$project" => {_id:0, userid: 1, email:1, name:1, timezone:1, username:1}}
+									])
+			
+			profileCount = userProfile.count
+			
+			if profileCount == 1
+				return userProfile[0]
+			elsif profileCount > 1
+				return "Oh oh, something went wrong. Multiple user counts were found that match your user ID."
+			elsif profileCount == 0
+				return "We could not find a user profile that matches your User ID"
+			end
+		end
+
+
+		# Checks MongoDB to see if the user has a record/profile based on the userid
+		def self.user_exists?(userid)
+			users = self.aggregate([
+									{ "$match" => {userid: userid}}
+									]).count
+			if users >= 1
+				return true
+			elsif users == 0 or users == nil
+				return false
+			elsif users > 1
+				return "Something went wrong... duplicate users have found in our records..."
+			end
+		end
+
+		# Returns true if the repo is already registered under the specific users
+		def self.repo_registered?(userid, repo)
+			repo = repo.downcase
+			repos = self.aggregate([
+									{ "$match" => {userid: userid}},
+									{ "$unwind" => "$registered_repos"},
+									# { "$project" => {"registered_repos.repo" => {"$toLower"=>"$registered_repos.repo"}}},
+									{ "$match" => {"registered_repos.repo" => repo}}
+									]).count
+									# ])
+			if repos == 1
+				return true
+			elsif repos == 0
+				return false
+			elsif repos > 1
+				# TODO add logic on app.rb side to account for the error message response.
+				return "Something went wrong...duplicate registered repository records have been found...."		
+			end
+		end
+
+
+		# Checks mongoDB for registered hooks for the user.
+		def self.reminder_hook_exists_in_mongo?(userid, repo)
+			repo = repo.downcase
+			hooks = self.aggregate([
+									{ "$match" => {userid: userid}},
+									{ "$unwind" => "$registered_hooks"},
+									# { "$project" => {"registered_hooks.repo" => {"$toLower"=>"$registered_hooks.repo"}}},
+									{ "$match" => {"registered_hooks.repo" => repo}}
+									]).count
+
+			if hooks == 1
+				return true
+			elsif hooks == 0
+				return false
+			elsif hooks > 1
+				# TODO add logic on app.rb side to account for the error message response.
+				return "Something went wrong...duplicate registered hook records have been found...."		
+			end
+		end
+
+
+end
+
+
 
 
 class CheckIfReminder
 	def self.perform(job)
 		# TODO rebuild this method so it chains multiple jobs together
-		commentAttrs = job.data[:comment].attrs
 		
-		if ReminderValidation.is_Reminder_Comment?(commentAttrs[:comment].attrs[:body]) == false
-			return "Not a Reminder Comment"
-		
-		elsif ReminderValidation.is_Reminder_Comment?(commentAttrs[:comment].attrs[:body]) == true
-			
-			# TODO Validation of Hook for Repo
-			# TODO Validation of Repo for user
+
+
+		commentData = JSON.parse(job.data["comment"])
+			commentBody = commentData["comment"]["body"]
+			userid = commentData["comment"]["user"]["id"]
+
+			issueNumber = commentData["issue"]["number"]
+			issueTitle = commentData["issue"]["title"]
+			commentID = commentData["comment"]["id"]
+			repoName = commentData["repository"]["name"]
+			repoFullName = commentData["repository"]["full_name"]
+
+
+
 
 			# if hook and repo for user is validated then
-				userTimezone = nil # Get user's timezone from mongo
-				userToEmail = nil # Get user's selected email from mongo
+				
 				calcDelay = nil # Calculate the number of seconds between the Comment Created_At DateTime and the Reminder DataTime
 				username = nil
 				repo = nil
 				tags = nil
 
 
-			parsedRemidner = ReminderValidation.process_request(job.data[:comment].attrs, userTimezone)	
-			
+		isReminderTF = ReminderValidation.is_Reminder_Comment?(commentBody)
+
+		if  isReminderTF == false
+			puts "Not a Reminder Comment"
+		
+		elsif isReminderTF == true
+			puts "Is a Reminder Comment"
 
 
-			if parsedRemidner.class == Hash
-				generatedSubject = nil
-				generatedBody = nil
+			hookExistsTF = MongoQueries.reminder_hook_exists_in_mongo?(userid, repoFullName)
+			if hookExistsTF == true
 
-				client = Qless::Client.new(:url => ENV["REDIS_URL"])
-				queue = client.queues['testing']
-				queue.put(SendEmail, {:toEmail => job.data["toEmail"] ||= "stephenrussett@gmail.com",
-										:body => jobs.data["body"] ||= "Test Body",
-										:subject => job.data["subject"] ||= "TestSubject"
-										}, 
-										:delay => job.data["delay"] ||= 0,
-										# :tags => ["User|#{job.data['username']}",
-										# 		 "Repo|#{job.data['repo']}",
-										# 		 "Issue|#{job.data['issueNumber']}"]
-										)
+
+				userExistsTF = MongoQueries.user_exists?(userid)
+
+				if userExistsTF == true
+					userProfile = MongoQueries.get_user_profile(userid)
+						userTimezone = userProfile["timezone"]
+						userToEmail = userProfile["email"]
+						userName = userProfile["name"]
+
+
+					# Validates that the user who created the comment has the repo registered
+					repoRegisteredTF = MongoQueries.repo_registered?(userid, repoFullName)
+					if repoRegisteredTF == true
+					# TODO Validation of Hook for Repo
+
+						parsedRemidner = ReminderValidation.process_request(job.data[:comment].attrs, userTimezone)	
+						
+						if parsedRemidner.class == Hash
+							generatedSubject = nil
+							generatedBody = nil
+
+							client = Qless::Client.new(:url => ENV["REDIS_URL"])
+							queue = client.queues['testing']
+							queue.put(SendEmail, {:toEmail => userToEmail,
+													:body => "My timezone is #{userTimezone}, My issueNumber: #{issueNumber}, My Issue Title: #{issueTitle}, My Comment ID: #{commentID}, My Repo Name: #{repoName}, My Full Repo Name: #{repoFullName}",
+													:subject => "GitHub-Reminder"
+													}, 
+													:delay => 0,
+													# :tags => ["User|#{job.data['username']}",
+													# 		 "Repo|#{job.data['repo']}",
+													# 		 "Issue|#{job.data['issueNumber']}"]
+													)
+						end
+					elsif repoRegisteredTF == false
+						puts "user did not have the repo registered"		
+					end
+				elsif userExistsTF == false
+					puts "user does not have a account"
+				end
+
+			elsif hookExistsTF == false
+				puts "hook does not exist"
 			end
-		end		
+
+		end
 	end
 end
 
