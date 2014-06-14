e85478fabd9da32eb72cb01ecaa0eae2b90cc857
Stephen Russett <stephenrussett@gmail.com>
2014-06-13 21:12:13 -0400

    added parsed comment output for debugging

--- jobs.rb
+++ jobs.rb
@@ -177,7 +177,7 @@ class CheckIfReminder
 							client = Qless::Client.new(:url => ENV["REDIS_URL"])
 							queue = client.queues['testing']
 							queue.put(SendEmail, {:toEmail => userToEmail,
-													:body => "My timezone is #{userTimezone}, My issueNumber: #{issueNumber}, My Issue Title: #{issueTitle}, My Comment ID: #{commentID}, My Repo Name: #{repoName}, My Full Repo Name: #{repoFullName}",
+													:body => "My timezone is #{userTimezone}, My issueNumber: #{issueNumber}, My Issue Title: #{issueTitle}, My Comment ID: #{commentID}, My Repo Name: #{repoName}, My Full Repo Name: #{repoFullName},    #{parsedReminder}",
 													:subject => "GitHub-Reminder"
 													}, 
 													:delay => 0,
