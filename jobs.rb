b588e4b56658a0b94f6e19f263a7c42859069a28
Stephen Russett <stephenrussett@gmail.com>
2014-06-18 21:30:30 -0400

    missed comma

--- jobs.rb
+++ jobs.rb
@@ -201,7 +201,7 @@ class CheckIfReminder
 
 							client = Qless::Client.new(:url => ENV["REDIS_URL"])
 							queue = client.queues['testing']
-							queue.put(SendEmail, { :parentValidationJobId => job.jid
+							queue.put(SendEmail, { :parentValidationJobId => job.jid,
 													:toEmail => userToEmail,
 													:body => emailBody,
 													# :body => "My timezone is #{userTimezone}, My issueNumber: #{issueNumber}, My Issue Title: #{issueTitle}, My Comment ID: #{commentID}, My Repo Name: #{repoName}, My Full Repo Name: #{repoFullName},    #{parsedReminder},  Comment Created At:  #{commentCreated_At},  #{Time.now}",
